0f09dcc0209c2db422480ff733347912564a2caa
BloodyRain2k <BloodyRain2k@fef01e1f-1acb-4511-8d79-f4a48d001ba2>
2013-03-18 01:42:12 +0000

    clamping to screen and disabled not working snapping
    git-svn-id: http://svn.mumech.com/MuMechLib/trunk@68 fef01e1f-1acb-4511-8d79-f4a48d001ba2

--- MuMechLib/DisplayModule.cs
+++ MuMechLib/DisplayModule.cs
@@ -15,11 +15,22 @@ namespace MuMech
             get { return new Rect(windowVector.x, windowVector.y, windowVector.z, windowVector.w); }
             set 
             { 
-                windowVector = new Vector4(value.x, value.y, value.width, value.height);
+                windowVector = new Vector4(
+            		Math.Min(Math.Max(value.x, 0), Screen.width - value.width),
+            		Math.Min(Math.Max(value.y, 0), Screen.height - value.height),
+            		value.width, value.height
+            	);
                 windowVector.x = Mathf.Clamp(windowVector.x, 10 - value.width, Screen.width - 10);
                 windowVector.y = Mathf.Clamp(windowVector.y, 10 - value.height, Screen.height - 10);
             }
         }
+        
+        public void SetPos(float x, float y)
+        {
+        	windowVector.x = Math.Min(Math.Max(x, 0), Screen.width - windowVector.z);
+        	windowVector.y = Math.Min(Math.Max(y, 0), Screen.height - windowVector.w);
+        }
+        
         [Persistent(pass = (int)Pass.Global)]
         public Vector4 windowVector; //Persistence is via a Vector4 since ConfigNode doesn't know how to serialize Rects
 
@@ -43,20 +54,41 @@ namespace MuMech
             GUI.DragWindow();
         }
 
-
         public virtual void DrawGUI(int baseWindowID, bool inEditor)
         {
             if (showInCurrentScene)
             {
                 windowPos = GUILayout.Window(baseWindowID, windowPos, WindowGUI, GetName(), WindowOptions());
-
+            	
                 if (GUI.Button(new Rect(windowPos.x + windowPos.width - 18, windowPos.y + 2, 16, 16), ""))
                 {
                     enabled = false;
                 }
+                
+//                var windows = core.GetComputerModules<DisplayModule>(); // on ice until there's a way to find which window is active, unless you like dragging other windows by snapping
+//                
+//                foreach (var w in windows)
+//                {
+//                	if (w == this) { continue; }
+//                	
+//                	var diffL = (w.windowPos.x + w.windowPos.width - 2) - windowPos.x;
+//                	var diffR = w.windowPos.x - (windowPos.x + windowPos.width + 2);
+//                	var diffT = (w.windowPos.y + w.windowPos.height - 2) - windowPos.y;
+//                	var diffB = w.windowPos.y - (windowPos.y + windowPos.height + 2);
+//                	
+//                	if (Math.Abs(diffL) <= 8)
+//                	{
+//                		SetPos(w.windowPos.x + w.windowPos.width - 2, windowPos.y);
+//                	}
+//                	
+//                	if (Math.Abs(diffR) <= 8)
+//                	{
+//                		SetPos(w.windowPos.x - windowPos.width + 2, windowPos.y);
+//                	}
+//                }
             }
         }
-
+        
         public override void OnSave(ConfigNode local, ConfigNode type, ConfigNode global)
         {
             base.OnSave(local, type, global);
