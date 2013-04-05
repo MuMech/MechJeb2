7476012b488354f26f72bbe06a4edac3c15c6373
Thiago Ramon Gonçalves Montoya <thiagoramon@gmail.com>
2013-04-04 22:40:02 -0300

    Preparing 2.0.4 release

--- MechJeb2/Properties/AssemblyInfo.cs
+++ MechJeb2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Multiversal Mechatronics")]
 [assembly: AssemblyProduct("MuMechLib")]
-[assembly: AssemblyCopyright("Copyright © Multiversal Mechatronics 2012")]
+[assembly: AssemblyCopyright("Copyright © Multiversal Mechatronics 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.3")]
-[assembly: AssemblyFileVersion("2.0.0.3")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
