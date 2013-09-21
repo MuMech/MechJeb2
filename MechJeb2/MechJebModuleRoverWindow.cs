9d77ef7ef48dbffd79a7e3478b9be33259ffb8ee
Bloodyrain <bloodyrain2k@gmx.de>
2013-09-21 20:18:44 +0200

    changed handling of target to be usable with the waypoint list no good means of altering the waypoint list yet, only drive towards a target which sets the target as only entry in the list or adding a target to the list

--- MechJeb2/MechJebModuleRoverWindow.cs
+++ MechJeb2/MechJebModuleRoverWindow.cs
@@ -6,54 +6,74 @@ using UnityEngine;
 
 namespace MuMech
 {
-    public class MechJebModuleRoverWindow : DisplayModule
-    {
-        public MechJebModuleRoverController autopilot;
+	public class MechJebModuleRoverWindow : DisplayModule
+	{
+		public MechJebModuleRoverController autopilot;
 
-        public MechJebModuleRoverWindow(MechJebCore core) : base(core) { }
+		public MechJebModuleRoverWindow(MechJebCore core) : base(core) { }
 
-        public override void OnStart(PartModule.StartState state)
-        {
-            autopilot = core.GetComputerModule<MechJebModuleRoverController>();
-        }
+		public override void OnStart(PartModule.StartState state)
+		{
+			autopilot = core.GetComputerModule<MechJebModuleRoverController>();
+		}
 
-        public override string GetName()
-        {
-            return "Rover Autopilot";
-        }
+		public override string GetName()
+		{
+			return "Rover Autopilot";
+		}
 
-        public override GUILayoutOption[] WindowOptions()
-        {
-            return new GUILayoutOption[] { GUILayout.Width(200), GUILayout.Height(50) };
-        }
+		public override GUILayoutOption[] WindowOptions()
+		{
+			return new GUILayoutOption[] { GUILayout.Width(200), GUILayout.Height(50) };
+		}
 
-        protected override void WindowGUI(int windowID)
-        {
-            MechJebModuleCustomWindowEditor ed = core.GetComputerModule<MechJebModuleCustomWindowEditor>();
+		protected override void WindowGUI(int windowID)
+		{
+			MechJebModuleCustomWindowEditor ed = core.GetComputerModule<MechJebModuleCustomWindowEditor>();
 
-            ed.registry.Find(i => i.id == "Toggle:RoverController.ControlHeading").DrawItem();
-            ed.registry.Find(i => i.id == "Editable:RoverController.heading").DrawItem();
-            ed.registry.Find(i => i.id == "Value:RoverController.headingErr").DrawItem();
-            ed.registry.Find(i => i.id == "Toggle:RoverController.ControlSpeed").DrawItem();
-            ed.registry.Find(i => i.id == "Editable:RoverController.speed").DrawItem();
-            ed.registry.Find(i => i.id == "Value:RoverController.speedErr").DrawItem();
+			ed.registry.Find(i => i.id == "Toggle:RoverController.ControlHeading").DrawItem();
+			ed.registry.Find(i => i.id == "Editable:RoverController.heading").DrawItem();
+			ed.registry.Find(i => i.id == "Value:RoverController.headingErr").DrawItem();
+			ed.registry.Find(i => i.id == "Toggle:RoverController.ControlSpeed").DrawItem();
+			ed.registry.Find(i => i.id == "Editable:RoverController.speed").DrawItem();
+			ed.registry.Find(i => i.id == "Value:RoverController.speedErr").DrawItem();
 
-            base.WindowGUI(windowID);
-        }
+			GUILayout.BeginHorizontal();
+			
+			if (core.target.Target != null && ((core.target.PositionTargetExists && core.target.targetBody == orbit.referenceBody) || core.target.Orbit.referenceBody == orbit.referenceBody)) {
+				if (GUILayout.Button("To Target")) {
+					autopilot.Waypoints.Clear();
+					var pos = (core.target.PositionTargetExists ? (Vector3)core.target.GetPositionTargetPosition() : core.target.Position);
+					autopilot.Waypoints.Add(new MechJebRoverWaypoint(pos));
+					autopilot.WaypointIndex = 0;
+					autopilot.ControlHeading = autopilot.ControlSpeed = true;
+				}
 
-        public override void OnUpdate()
-        {
-            if (autopilot != null)
-            {
-                if (autopilot.ControlHeading || autopilot.ControlSpeed)
-                {
-                    autopilot.users.Add(this);
-                }
-                else
-                {
-                    autopilot.users.Remove(this);
-                }
-            }
-        }
-    }
+				if (GUILayout.Button("Add Target")) {
+					var pos = (core.target.PositionTargetExists ? (Vector3)core.target.GetPositionTargetPosition() : core.target.Position);
+					autopilot.Waypoints.Add(new MechJebRoverWaypoint(pos));
+					if (autopilot.WaypointIndex < 0) { autopilot.WaypointIndex = autopilot.Waypoints.Count - 1; }
+				}
+			}
+			
+			GUILayout.EndHorizontal();
+			
+			base.WindowGUI(windowID);
+		}
+
+		public override void OnUpdate()
+		{
+			if (autopilot != null)
+			{
+				if (autopilot.ControlHeading || autopilot.ControlSpeed)
+				{
+					autopilot.users.Add(this);
+				}
+				else
+				{
+					autopilot.users.Remove(this);
+				}
+			}
+		}
+	}
 }
