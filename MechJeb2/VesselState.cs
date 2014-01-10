c6a4ac9b28c2dee32bfeab9f0d848352299f90e3
Sarbian <Sarbian@sarbian.com>
2014-01-10 18:42:59 +0100

    Torque from gimbal used current thrust but function calling it multiply by current thrust too

--- MechJeb2/VesselState.cs
+++ MechJeb2/VesselState.cs
@@ -632,7 +632,7 @@ namespace MuMech
                     if (gimbal != null && !gimbal.gimbalLock)
                     {
                         double gimbalRange = gimbal.gimbalRange;
-                        torqueThrustPYAvailable += Math.Sin(Math.Abs(gimbalRange) * Math.PI / 180) * eCurrentThrust * (p.Rigidbody.worldCenterOfMass - CoM).magnitude; // TODO: close enough?
+                        torqueThrustPYAvailable += Math.Sin(Math.Abs(gimbalRange) * Math.PI / 180) * e.maxThrust * usableFraction * (p.Rigidbody.worldCenterOfMass - CoM).magnitude; // TODO: close enough?
                     }
                 }
             }
