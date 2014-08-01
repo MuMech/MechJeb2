541c4dbb4ddd3f824154b71ac88276da9665d336
Anatid <theduck314@gmail.com>
2014-07-31 22:59:49 -0400

    Simulation ignores engines limited to zero thrust (fixes #395)

--- MechJeb2/FuelFlowSimulation.cs
+++ MechJeb2/FuelFlowSimulation.cs
@@ -339,7 +339,7 @@ namespace MuMech
             if (engine != null)
             {
                 //Only count engines that either are ignited or will ignite in the future:
-                if (HighLogic.LoadedSceneIsEditor || inverseStage < Staging.CurrentStage || engine.getIgnitionState)
+                if ((HighLogic.LoadedSceneIsEditor || inverseStage < Staging.CurrentStage || engine.getIgnitionState) && engine.thrustPercentage > 0)
                 {
                     //if an engine has been activated early, pretend it is in the current stage:
                     if (engine.getIgnitionState && inverseStage < Staging.CurrentStage)
@@ -383,7 +383,7 @@ namespace MuMech
             if (enginefx != null)
             {
                 //Only count engines that either are ignited or will ignite in the future:
-                if (HighLogic.LoadedSceneIsEditor || inverseStage < Staging.CurrentStage || enginefx.getIgnitionState)
+                if ((HighLogic.LoadedSceneIsEditor || inverseStage < Staging.CurrentStage || enginefx.getIgnitionState) && engine.thrustPercentage > 0)
                 {
                     //if an engine has been activated early, pretend it is in the current stage:
                     if (enginefx.getIgnitionState && inverseStage < Staging.CurrentStage)
