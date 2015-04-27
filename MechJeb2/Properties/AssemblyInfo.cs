5a679f322bcd12a331664e68fa7c65a4474ed173
Thiago Ramon Gonçalves Montoya <thiagoramon@gmail.com>
2015-04-27 20:55:16 -0300

    More changes to landing autopilot; Preparing 2.5 release;

--- MechJeb2/Properties/AssemblyInfo.cs
+++ MechJeb2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Multiversal Mechatronics")]
 [assembly: AssemblyProduct("MuMechLib")]
-[assembly: AssemblyCopyright("Copyright © Multiversal Mechatronics 2014")]
+[assembly: AssemblyCopyright("Copyright © Multiversal Mechatronics 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.0")]     // We should not change it anymore. It break mods that links MJ ( cf http://support.microsoft.com/kb/556041 )
-[assembly: AssemblyFileVersion("2.4.2.0")] // this one we can change all we want
+[assembly: AssemblyVersion("2.5.0.0")]     // We should not change it anymore. It break mods that links MJ ( cf http://support.microsoft.com/kb/556041 )
+[assembly: AssemblyFileVersion("2.5.0.0")] // this one we can change all we want
 [assembly: AssemblyInformationalVersion("")] // Displayed in the window title if not empty (used to display dev #)
 
-[assembly: KSPAssembly("MechJeb2", 2, 4)]
+[assembly: KSPAssembly("MechJeb2", 2, 5)]
