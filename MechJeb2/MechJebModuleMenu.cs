c5cd73b1b1893abfa61834e32f6d1be67ec13c66
Sarbian <Sarbian@Sarbian.com>
2016-01-22 21:27:43 +0100

    Remove some exception when killing the game

--- MechJeb2/MechJebModuleMenu.cs
+++ MechJeb2/MechJebModuleMenu.cs
@@ -245,7 +245,7 @@ namespace MuMech
 
         private void ClearButtons()
         {
-            if (mjButton != null)
+            if (mjButton != null && mjButton.gameObject != null && mjButton.gameObject.activeInHierarchy) 
             {
                 ApplicationLauncher.Instance.RemoveModApplication(mjButton);
                 mjButton = null;
