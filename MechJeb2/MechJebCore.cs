d785d6b4c791cd2397634c06e4e042760b55bd71
Starwaster <starwaster@gmail.com>
2016-04-29 03:27:52 -0400

    fix wandering camera when mouse over MJ menu
    * fix wandering camera when mouse over MJ menu. (change locked ControlTypes to ControlTypes.ALLBUTCAMERA)

--- MechJeb2/MechJebCore.cs
+++ MechJeb2/MechJebCore.cs
@@ -1101,7 +1101,7 @@ namespace MuMech
             bool mouseOverWindow = GuiUtils.MouseIsOverWindow(this);
             if (!weLockedInputs && mouseOverWindow && !Input.GetMouseButton(1))
             {
-                InputLockManager.SetControlLock(ControlTypes.CAMERACONTROLS | ControlTypes.MAP, "MechJeb_noclick");
+                InputLockManager.SetControlLock(ControlTypes.ALLBUTCAMERAS, "MechJeb_noclick");
                 weLockedInputs = true;
             }
             if (weLockedInputs && !mouseOverWindow)
