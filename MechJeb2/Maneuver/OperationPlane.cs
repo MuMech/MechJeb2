1f37ba05b39a61d1c571be58eb9d45163fc074b8
formicant <formicant@gmail.com>
2016-05-30 00:48:31 +0600

    Add ‘At the highest AN/DN’ and ‘At the nearest AN/DN’ time selectors for inclination and plane maneuvers. Change ‘Atmospheric pressure (Pa)’ format to ValueInfoItem.SI to prevent line wrapping.

--- MechJeb2/Maneuver/OperationPlane.cs
+++ MechJeb2/Maneuver/OperationPlane.cs
@@ -8,7 +8,11 @@
 
         public OperationPlane ()
         {
-            timeSelector = new TimeSelector(new TimeReference[] { TimeReference.REL_ASCENDING, TimeReference.REL_DESCENDING });
+            timeSelector = new TimeSelector(new TimeReference[]
+            {
+                TimeReference.REL_HIGHEST_AD, TimeReference.REL_NEAREST_AD,
+                TimeReference.REL_ASCENDING, TimeReference.REL_DESCENDING
+            });
         }
 
         public override void DoParametersGUI(Orbit o, double universalTime, MechJebModuleTargetController target)
@@ -28,24 +32,71 @@
             {
                 throw new OperationException("can only match planes with an object in the same sphere of influence.");
             }
-            else if (timeSelector.timeReference == TimeReference.REL_ASCENDING)
+
+            var anExists = o.AscendingNodeExists(target.TargetOrbit);
+            var dnExists = o.DescendingNodeExists(target.TargetOrbit);
+            double anTime = 0;
+            double dnTime = 0;
+            var anDeltaV = anExists ? OrbitalManeuverCalculator.DeltaVAndTimeToMatchPlanesAscending(o, target.TargetOrbit, UT, out anTime) : Vector3d.zero;
+            var dnDeltaV = anExists ? OrbitalManeuverCalculator.DeltaVAndTimeToMatchPlanesDescending(o, target.TargetOrbit, UT, out dnTime) : Vector3d.zero;
+            Vector3d dV;
+
+            if(timeSelector.timeReference == TimeReference.REL_ASCENDING)
             {
-                if (!o.AscendingNodeExists(target.TargetOrbit))
+                if(!anExists)
                 {
                     throw new OperationException("ascending node with target doesn't exist.");
+                }
+                UT = anTime;
+                dV = anDeltaV;
+            }
+            else if(timeSelector.timeReference == TimeReference.REL_DESCENDING)
+            {
+                if(!dnExists)
+                {
+                    throw new OperationException("descending node with target doesn't exist.");
+                }
+                UT = dnTime;
+                dV = dnDeltaV;
+            }
+            else if(timeSelector.timeReference == TimeReference.REL_NEAREST_AD)
+            {
+                if(!anExists && !dnExists)
+                {
+                    throw new OperationException("neither ascending nor descending node with target exists.");
+                }
+                if(!dnExists || anTime <= dnTime)
+                {
+                    UT = anTime;
+                    dV = anDeltaV;
+                }
+                else
+                {
+                    UT = dnTime;
+                    dV = dnDeltaV;
+                }
+            }
+            else if(timeSelector.timeReference == TimeReference.REL_HIGHEST_AD)
+            {
+                if(!anExists && !dnExists)
+                {
+                    throw new OperationException("neither ascending nor descending node with target exists.");
+                }
+                if(!dnExists || anDeltaV.magnitude <= dnDeltaV.magnitude)
+                {
+                    UT = anTime;
+                    dV = anDeltaV;
+                }
+                else
+                {
+                    UT = dnTime;
+                    dV = dnDeltaV;
                 }
             }
             else
             {
-                if (!o.DescendingNodeExists(target.TargetOrbit))
-                {
-                    throw new OperationException("descending node with target doesn't exist.");
-                }
+                throw new OperationException("wrong time reference.");
             }
-
-            Vector3d dV = (timeSelector.timeReference == TimeReference.REL_ASCENDING) ?
-                OrbitalManeuverCalculator.DeltaVAndTimeToMatchPlanesAscending(o, target.TargetOrbit, UT, out UT):
-                OrbitalManeuverCalculator.DeltaVAndTimeToMatchPlanesDescending(o, target.TargetOrbit, UT, out UT);
 
             return new ManeuverParameters(dV, UT);
         }
