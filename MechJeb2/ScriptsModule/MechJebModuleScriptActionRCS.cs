dfd544f47c41228048665d0556d9159006fb5919
Sébastien Bock <sebastienbock@sbock-wifi.aix.nextperf.local>
2016-10-21 18:00:26 +0200

    Add RCSThrottle and RCS for rotation in RCS action

--- MechJeb2/ScriptsModule/MechJebModuleScriptActionRCS.cs
+++ MechJeb2/ScriptsModule/MechJebModuleScriptActionRCS.cs
@@ -10,24 +10,55 @@ namespace MuMech
 
 		[Persistent(pass = (int)Pass.Type)]
 		private int actionType;
+		[Persistent(pass = (int)Pass.Type)]
+		private int actionObject;
 		private List<String> actionTypes = new List<String>();
+		private List<String> actionObjects = new List<String>();
 
 		public MechJebModuleScriptActionRCS (MechJebModuleScript scriptModule, MechJebCore core):base(scriptModule, core, NAME)
 		{
 			actionTypes.Add("Enable");
 			actionTypes.Add("Disable");
+			actionObjects.Add("RCS");
+			actionObjects.Add("RCS throttle when engines are offline");
+			actionObjects.Add("Use RCS for rotation");
 		}
 
 		override public void activateAction(int actionIndex)
 		{
 			base.activateAction(actionIndex);
-			if (actionType == 0)
+			if (actionObject == 0)
 			{
-				core.rcs.enabled = true;
+				if (actionType == 0)
+				{
+					core.rcs.enabled = true;
+				}
+				else
+				{
+					core.rcs.enabled = false;
+				}
 			}
-			else
+			else if (actionObject == 1)
 			{
-				core.rcs.enabled = false;
+				if (actionType == 0)
+				{
+					core.rcs.rcsThrottle = true;
+				}
+				else
+				{
+					core.rcs.rcsThrottle = false;
+				}
+			}
+			else if (actionObject == 2)
+			{
+				if (actionType == 0)
+				{
+					core.rcs.rcsForRotation = true;
+				}
+				else
+				{
+					core.rcs.rcsForRotation = false;
+				}
 			}
 			this.endAction();
 		}
@@ -40,8 +71,8 @@ namespace MuMech
 		override public void WindowGUI(int windowID) {
 			base.preWindowGUI(windowID);
 			base.WindowGUI(windowID);
-			GUILayout.Label ("RCS");
 			actionType = GuiUtils.ComboBox.Box(actionType, actionTypes.ToArray(), actionTypes);
+			actionObject = GuiUtils.ComboBox.Box(actionObject, actionObjects.ToArray(), actionObjects);
 			base.postWindowGUI(windowID);
 		}
 	}
