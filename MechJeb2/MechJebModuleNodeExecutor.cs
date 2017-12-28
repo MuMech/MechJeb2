c059c95a0414a1eb4fde3081add7a0ab35647ffb
Lamont Granquist <lamont@scriptkiddie.org>
2017-12-27 19:50:18 -0500

    angmom -> angvel

--- MechJeb2/MechJebModuleNodeExecutor.cs
+++ MechJeb2/MechJebModuleNodeExecutor.cs
@@ -161,7 +161,6 @@ namespace MuMech
                 }
                 else
                 {
-                    Debug.Log("angle = " + core.attitude.attitudeAngleFromTarget() + " angmom = " + core.vessel.angularMomentum.magnitude);
                     if (!MuUtils.PhysicsRunning() && core.attitude.attitudeAngleFromTarget() > 10 && timeToPEGEnable < 600)
                     {
                         //realign
