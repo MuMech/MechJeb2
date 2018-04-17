39633014267c984b67a22067b594066474a74732
Lamont Granquist <lamont@scriptkiddie.org>
2018-04-16 21:12:45 -0700

    Add xunit test framework
    this is pretty rough right now, but it runs dummytest.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

---
+++ MechJeb2Tests/DummyTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace B9PartSwitchTests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
