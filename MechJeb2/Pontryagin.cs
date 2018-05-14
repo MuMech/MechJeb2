dc2d4ff53dfc78bf38859991cff6a1b463a0b691
Lamont Granquist <lamont@scriptkiddie.org>
2018-05-14 14:34:59 -0700

    another quicksave
    all kinds of overhaul, all the tests broken
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- MechJeb2/Pontryagin.cs
+++ MechJeb2/Pontryagin.cs
@@ -1,44 +1,93 @@
-
 using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MuMech {
+    public enum ArcType { COAST, BURN };
+
     public class Pontryagin {
-        public struct Engine
+        public struct Arc
         {
-            public double mdot, thrust;
-            public Engine(double mdot, double thrust)
+            public double isp, thrust;
+            public double At, c;
+            public ArcType type;
+
+            public Arc(ArcType type, double isp = 0, double thrust = 0)
             {
-                this.mdot = mdot;
+                if (type == ArcType.COAST && ( isp != 0 || thrust != 0 ) )
+                    throw new Exception("Non zero isp and/or thrust given to coast arc");
+
+                if (type == ArcType.BURN && ( isp  == 0 || thrust == 0 ) )
+                    throw new Exception("Zero isp and/or thrust given to burn arc");
+
+                this.isp = isp;
                 this.thrust = thrust;
+                this.type = type;
+                this.At = this.c = 0;
             }
         }
 
-        public struct Problem
-        {
-            public Vector3d r0;
-            public Vector3d v0;
-            public double m0;
-            public Action<double[], double[]> bcfun;
+        public List<Arc> arcs;
+        public Vector3d r0, r0_bar;
+        public Vector3d v0, v0_bar;
+        public double m0;
+        public double mu;
+        public Action<double[], double[]> bcfun;
+        public const double g0 = 9.80665;
 
-            public Problem(Vector3d r0, Vector3d v0, double m0, Action<double[], double[]> bcfun)
-            {
-                this.r0 = r0;
-                this.v0 = v0;
-                this.m0 = m0;
-                this.bcfun = bcfun;
-            }
-        }
-
+        // this constructor is mostly intended for testing
         public Pontryagin()
         {
         }
 
+        public Pontryagin(Vector3d r0, Vector3d v0, double m0, double mu, List<Arc> arcs = null)
+        {
+            if (arcs == null)
+                this.arcs = new List<Arc>();
+            else
+                this.arcs = arcs;
+            this.r0 = r0;
+            this.v0 = v0;
+            this.m0 = m0;
+            this.mu = mu;
+        }
+
+        public double g_bar, r_scale, v_scale, t_scale;  /* scaling constants */
+
+        public void Normalize()
+        {
+            double r0m = r0.magnitude;
+            g_bar = mu / ( r0m * r0m );
+            r_scale = r0m;
+            v_scale = Math.Sqrt( r0m * g_bar );
+            t_scale = Math.Sqrt( r0m / g_bar );
+            r0_bar = r0 / r_scale;
+            v0_bar = v0 / v_scale;
+            for(int i = 0; i < arcs.Count; i++)
+            {
+                Arc arc = arcs[i];
+                arc.c = arc.isp / Math.Sqrt( r_scale / ( g0 * g0 * g0 ) );
+            }
+        }
+
+        public void UpdateCurrentState(Vector3d r0, Vector3d v0, double m0)
+        {
+            this.r0 = r0;
+            this.v0 = v0;
+            this.m0 = m0;
+        }
+
+        public void AddArc(Arc arc) {
+            arcs.Add(arc);
+        }
+
+        public int numArcs { get { return arcs.Count; } }
+
         public void centralForceThrust(double[] y, double x, double[] dy, object o)
         {
-            Engine e = (Engine)o;
-            double Fm = e.thrust / y[12];
+            Arc arc = (Arc)o;
+            double At = arc.thrust / ( y[12] * g0 );
             double r2 = y[0]*y[0] + y[1]*y[1] + y[2]*y[2];
             double r = Math.Sqrt(r2);
             double r3 = r2 * r;
@@ -50,10 +99,10 @@ namespace MuMech {
             dy[0] = y[3];
             dy[1] = y[4];
             dy[2] = y[5];
-            /* dv = - r / r^3 + Fm * u */
-            dy[3] = - y[0] / r3 + Fm * y[6] / pvm;
-            dy[4] = - y[1] / r3 + Fm * y[7] / pvm;
-            dy[5] = - y[2] / r3 + Fm * y[8] / pvm;
+            /* dv = - r / r^3 + At * u */
+            dy[3] = - y[0] / r3 + At * y[6] / pvm;
+            dy[4] = - y[1] / r3 + At * y[7] / pvm;
+            dy[5] = - y[2] / r3 + At * y[8] / pvm;
             /* dpv = - pr */
             dy[6] = - y[9];
             dy[7] = - y[10];
@@ -63,34 +112,42 @@ namespace MuMech {
             dy[10] = y[7] / r3 - 3 / r5 * rdotpv * y[1];
             dy[11] = y[8] / r3 - 3 / r5 * rdotpv * y[2];
             /* m = mdot */
-            dy[12] = - e.mdot;
+            dy[12] = - arc.thrust / arc.c;
             /* FIXME: should throw if we get down to less than a kg or something */
         }
 
         // free attachment into the orbit defined by rT + vT
-        public void terminal5constraint(double[] yT, double[] z, Vector3d rT, Vector3d vT)
+        public void terminal5constraint( Vector3d rT, Vector3d vT)
         {
+            bcfun = (double[] yT, double[] zterm) => terminal5constraint(yT, zterm, rT, vT);
+        }
+
+        private void terminal5constraint(double[] yT, double[] z, Vector3d rT, Vector3d vT)
+        {
+            Vector3d rT_bar = rT / r_scale;
+            Vector3d vT_bar = vT / v_scale;
+
             Vector3d rf = new Vector3d(yT[0], yT[1], yT[2]);
             Vector3d vf = new Vector3d(yT[3], yT[4], yT[5]);
             Vector3d pvf = new Vector3d(yT[6], yT[7], yT[8]);
             Vector3d prf = new Vector3d(yT[9], yT[10], yT[11]);
 
-            Vector3d hT = Vector3d.Cross(rT, vT);
+            Vector3d hT = Vector3d.Cross(rT_bar, vT_bar);
 
             if (hT[2] == 0)
             {
                 /* degenerate inc=90 case so swap all the coordinates */
                 rf = rf.Reorder(231);
                 vf = vf.Reorder(231);
-                rT = rT.Reorder(231);
-                vT = vT.Reorder(231);
+                rT_bar = rT_bar.Reorder(231);
+                vT_bar = vT_bar.Reorder(231);
                 prf = prf.Reorder(231);
                 pvf = pvf.Reorder(231);
-                hT = Vector3d.Cross(rT, vT);
+                hT = Vector3d.Cross(rT_bar, vT_bar);
             }
 
             Vector3d hf = Vector3d.Cross(rf, vf);
-            Vector3d eT = - ( rT.normalized + Vector3d.Cross(hT, vT) );
+            Vector3d eT = - ( rT_bar.normalized + Vector3d.Cross(hT, vT_bar) );
             Vector3d ef = - ( rf.normalized + Vector3d.Cross(hf, vf) );
             Vector3d hmiss = hf - hT;
             Vector3d emiss = ef - eT;
@@ -104,7 +161,7 @@ namespace MuMech {
             z[5] = trans;
         }
 
-        public void singleIntegrate(double[] y0, double[] yf, int n, ref double t, double dt, Engine e)
+        public void singleIntegrate(double[] y0, double[] yf, int n, ref double t, double dt, Arc e)
         {
             double eps = 0.00001;
             double h = 0;
@@ -127,40 +184,41 @@ namespace MuMech {
             t = t + dt;
         }
 
-        public void multipleIntegrate(double[] y0, double[] yf, Problem p)
+        public void multipleIntegrate(double[] y0, double[] yf) // , Problem p)
         {
             double tc = y0[26];
             double tb = y0[27];
             double t = 0;
 
-            Engine e = new Engine(0, 0);
-            singleIntegrate(y0, yf, 0, ref t, tc, e);
+            Arc arc = new Arc(ArcType.COAST);
+            singleIntegrate(y0, yf, 0, ref t, tc, arc);
 
-            e = new Engine(63136.1585987428, 25092.0703945434);
-            singleIntegrate(y0, yf, 1, ref t, tb, e);
+            arc = new Arc(ArcType.BURN, mdot: 63136.1585987428, thrust: 25092.0703945434);
+            singleIntegrate(y0, yf, 1, ref t, tb, arc);
         }
 
         public void optimizationFunction(double[] y0, double[] z, object o)
         {
-            Problem p = (Problem)o;
-
             double[] yf = new double[26];
-            multipleIntegrate(y0, yf, p);
+            multipleIntegrate(y0, yf);
 
             /* initial conditions */
-            z[0] = y0[0] - p.r0[0];
-            z[1] = y0[1] - p.r0[1];
-            z[2] = y0[2] - p.r0[2];
-            z[3] = y0[3] - p.v0[0];
-            z[4] = y0[4] - p.v0[1];
-            z[5] = y0[5] - p.v0[2];
-            z[6] = y0[12] - p.m0;
+            z[0] = y0[0] - r0[0];
+            z[1] = y0[1] - r0[1];
+            z[2] = y0[2] - r0[2];
+            z[3] = y0[3] - v0[0];
+            z[4] = y0[4] - v0[1];
+            z[5] = y0[5] - v0[2];
+            z[6] = y0[12] - m0;
 
             /* terminal constraints */
             double[] yT = new double[13];
             Array.Copy(yf, 13, yT, 0, 13);
             double[] zterm = new double[6];
-            p.bcfun(yT, zterm);
+            if ( bcfun == null )
+                throw new Exception("No bcfun was provided to the Pontryagin optimizer");
+
+            bcfun(yT, zterm);
 
             z[7] = zterm[0];
             z[8] = zterm[1];
@@ -192,15 +250,17 @@ namespace MuMech {
                 z[i] = z[i] * z[i];
         }
 
-        public void Optimize(double[] y0, double[] yf, Problem p)
+
+        public void Optimize(double[] y0, double[] yf)
         {
+            Normalize();
             alglib.minlmstate state;
             alglib.minlmreport rep = new alglib.minlmreport();
             alglib.minlmcreatev(28, 28, y0, 0.001, out state);  /* TODO: what should diffstate be? */
             alglib.minlmsetcond(state, 0.00001, 1000);
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            alglib.minlmoptimize(state, optimizationFunction, null, p);
+            alglib.minlmoptimize(state, optimizationFunction, null, null);
             alglib.minlmresultsbuf(state, ref yf, rep);
             stopWatch.Stop();
             Console.WriteLine("Report Code: " + rep.terminationtype);
