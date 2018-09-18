2dfb9f329cebf6cfaabd6ef0f54a0c817dc4d9c7
Lamont Granquist <lamont@scriptkiddie.org>
2018-09-18 15:12:12 -0700

    force throttle limiters off and remove debugging
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- MechJeb2/MechJebModuleAscentGuidance.cs
+++ MechJeb2/MechJebModuleAscentGuidance.cs
@@ -224,6 +224,14 @@ namespace MuMech
                     else
                     {
                         GUILayout.Label("FIXME: throttle limiters are down for maintenance");
+                        core.thrust.limitToPreventOverheats = false;
+                        core.thrust.limitDynamicPressure = false;
+                        core.thrust.limitAcceleration = false;
+                        core.thrust.limitThrottle = false;
+                        core.thrust.limiterMinThrottle = false;
+                        core.thrust.limitToTerminalVelocity = false;
+                        core.thrust.electricThrottle = false;
+
                     }
                     GUILayout.BeginHorizontal();
                     autopilot.forceRoll = GUILayout.Toggle(autopilot.forceRoll, "Force Roll");
