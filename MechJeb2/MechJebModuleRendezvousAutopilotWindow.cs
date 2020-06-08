1bb6095fe4c2ec39549a543a4f5f778cbd4dcb00
Felix Scheffer <mail@felix-scheffer.de>
2020-06-09 00:50:15 +0200

    #1059 - adds a field for max closing velocity in Rendezvous Autopilot

--- MechJeb2/MechJebModuleRendezvousAutopilotWindow.cs
+++ MechJeb2/MechJebModuleRendezvousAutopilotWindow.cs
@@ -44,6 +44,7 @@ namespace MuMech
 
                 GuiUtils.SimpleTextBox(Localizer.Format("#MechJeb_RZauto_label4"), autopilot.desiredDistance, "m");//"Desired final distance:"
                 GuiUtils.SimpleTextBox(Localizer.Format("#MechJeb_RZauto_label5"), autopilot.maxPhasingOrbits);//"Max # of phasing orbits:"
+                GuiUtils.SimpleTextBox(Localizer.Format("#MechJeb_RZauto_label8"), autopilot.maxClosingSpeed, "m/s");//"Max closing velocity:"
 
                 if (autopilot.maxPhasingOrbits < 5)
                 {
