b04390740b1478efdda6169ae9ca7e08dac12dfd
Lamont Granquist <lamont@scriptkiddie.org>
2022-04-16 11:14:26 -0700

    Remove the tests
    I didn't know what I was doing when I did this.
    
    Should do it, but need to revisit it later, and might as well
    start over from scratch.
    
    C# is an IDE-centric language and managing csproj files
    from the CLI+vim is just endless pain.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- MechJeb2Tests/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style","IDE0055:Fix formatting",Justification = "<Pending>")]
-[assembly: SuppressMessage("Style","IDE1006:Naming Styles",Justification = "<Pending>")]
-[assembly: SuppressMessage("CodeQuality","IDE0079:Remove unnecessary suppression",Justification = "<Pending>")]
