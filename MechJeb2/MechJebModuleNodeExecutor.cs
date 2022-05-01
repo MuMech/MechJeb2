e392c1aa510c249f0bd75d90a2aa7134651263c6
NathanKell <nathankell@gmail.com>
2022-04-30 21:54:33 -0700

    Fix Principia node execution warpto bug (was warping to relative time-to-node, not to absolute UT).

--- MechJeb2/MechJebModuleNodeExecutor.cs
+++ MechJeb2/MechJebModuleNodeExecutor.cs
@@ -174,7 +174,7 @@ namespace MuMech
                         ? core.attitude.attitudeAngleFromTarget() < 1 && core.vessel.angularVelocity.magnitude < 0.001
                         : core.attitude.attitudeAngleFromTarget() < 10))
                     {
-                        core.warp.WarpToUT(timeToNode - spool - leadTime);
+                        core.warp.WarpToUT(timeToNode + vesselState.time - spool - leadTime);
                     }
                     else
                     {
