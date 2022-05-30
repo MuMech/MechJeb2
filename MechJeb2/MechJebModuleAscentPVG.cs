MuMech/MechJeb2

commit 136964af2380013e860a04e0433fe6a607874847

Just a bit of cleanup
Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

MechJeb2/MechJebModuleAscentPVG.cs
@@ -117,11 +117,14 @@ namespace MuMech
 
         private void setTarget()
         {
+            bool lanflag = false;
+
             double sma = 0;
             double ecc = 0;
             double vT = 0;
             double rT = 0;
             double gammaT = 0;
+            double LAN = 0;
 
             ConvertToSMAEcc(autopilot.desiredOrbitAltitude, desiredApoapsis, out sma, out ecc);
             ConvertToVTRT(sma, ecc, attachAltFlag ? desiredAttachAlt : autopilot.desiredOrbitAltitude, out gammaT, out rT, out vT);
@@ -129,24 +132,24 @@ namespace MuMech
 
             if ( ascentGuidance.launchingToPlane && core.target.NormalTargetExists )
             {
-                double LAN = core.target.TargetOrbit.LAN;
+                LAN = core.target.TargetOrbit.LAN;
                 inclination = core.target.TargetOrbit.inclination;
-                if (ecc < 1e-4 || attachAltFlag)
-                    core.guidance.flightangle5constraint(rT, vT, inclination, gammaT, LAN, sma, omitCoast, true, true);
-                else
-                    core.guidance.keplerian4constraintArgPfree(sma, ecc, inclination, LAN, omitCoast, true, true);
+                lanflag = true;
             }
             else if ( ascentGuidance.launchingToMatchLAN && core.target.NormalTargetExists )
             {
-                double LAN = core.target.TargetOrbit.LAN;
-                if (ecc < 1e-4 || attachAltFlag)
-                    core.guidance.flightangle5constraint(rT, vT, inclination, gammaT, LAN, sma, omitCoast, false, true);
-                else
-                    core.guidance.keplerian4constraintArgPfree(sma, ecc, inclination, LAN, omitCoast, false, true);
+                LAN = core.target.TargetOrbit.LAN;
+                lanflag = true;
+
             }
             else if ( ascentGuidance.launchingToLAN )
             {
-                double LAN = autopilot.desiredLAN;
+                LAN = autopilot.desiredLAN;
+                lanflag = true;
+            }
+
+            if ( lanflag )
+            {
                 if (ecc < 1e-4 || attachAltFlag)
                     core.guidance.flightangle5constraint(rT, vT, inclination, gammaT, LAN, sma, omitCoast, false, true);
                 else

commit 277a415d834a519e7f82e395892cff8ad671f4eb

Fix Launch-into-plane-of-target with Principia
When launching into the plane of a target with Principia installed, PVG
gets stuck on 'INITIALIZING' and never actually launches.

This was previously fixed in cd3d194760f7875658e7e04690e37113d2ca1cd6 by
adding the `targetInc` parameter to the PVG constraint setup functions,
but the refactor in 136964af2380013e860a04e0433fe6a607874847 broke this.

MechJeb2/MechJebModuleAscentPVG.cs
@@ -128,6 +128,7 @@ namespace MuMech
         private void SetTarget()
         {
             bool lanflag = false;
+            bool targetInc = false;
 
             double sma = 0;
             double ecc = 0;
@@ -150,6 +151,7 @@ namespace MuMech
             {
                 LAN         = core.target.TargetOrbit.LAN;
                 inclination = Math.Sign(inclination) * core.target.TargetOrbit.inclination;
+                targetInc   = true;
                 lanflag     = true;
             }
             else if (AscentGuidance.launchingToMatchLAN && core.target.NormalTargetExists)
@@ -170,9 +172,9 @@ namespace MuMech
             if (lanflag)
             {
                 if (ecc < 1e-4 || AttachAltFlag)
-                    core.guidance.flightangle5constraint(rT, vT, inclination, gammaT, LAN, sma, coastLen, false, true);
+                    core.guidance.flightangle5constraint(rT, vT, inclination, gammaT, LAN, sma, coastLen, targetInc, true);
                 else
-                    core.guidance.keplerian4constraintArgPfree(sma, ecc, inclination, LAN, coastLen, false, true);
+                    core.guidance.keplerian4constraintArgPfree(sma, ecc, inclination, LAN, coastLen, targetInc, true);
             }
             else
             {