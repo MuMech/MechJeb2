118a7971a6b659bb6ec72173081c1a2dbd90ed13
Lamont Granquist <lamont@scriptkiddie.org>
2023-06-16 17:39:41 -0700

    PVG cleanup: replace ResidualWrapper with ContinuityLayout/ResidualLayout
    also cleans up the "Problem" a bit and removes that one last annoying
    null warning i've left in for about a year.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- MechJeb2/MechJebLib/PVG/InputLayout.cs
+++ MechJeb2/MechJebLib/PVG/InputLayout.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Copyright Lamont Granquist, Sebastien Gaggini and the MechJeb contributors
+ * SPDX-License-Identifier: LicenseRef-PD-hp OR Unlicense OR CC0-1.0 OR 0BSD OR MIT-0 OR MIT OR LGPL-2.1+
+ */
+
+#nullable enable
+
+using System;
 using System.Collections.Generic;
 using MechJebLib.Primitives;
 
